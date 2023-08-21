abafa35cad741b930f7ae5e9a33f27fcf63fec7f
tcmbonani <tcmbonani@gmail.com>
2023-08-21 02:36:07 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -23,3 +23,48 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
+	va_start(list, format);
+
+	count = 0;
+
+	for (i = 0; *(format + i); i++)
+	{
+		if (*(format + i) == '%')
+		{
+			if (*(format + i + 1) == '\0')
+				continue;
+			find = 0;
+
+			for (j = 0; j < 6; j++)
+			{
+				if (*(format + i + 1) == arguments[j].spec)
+				{
+					count += arguments[j].print(list);
+					find = 1;
+					format++;
+					break;
+				}
+			}
+
+			if (find != 1)
+			{
+				if (*(format + i + 1) == '%')
+				{
+					count += write(1, "%", 1);
+					format++;
+				}
+				else
+					count += write(1, (format + i), 1);
+			}
+
+		}
+		else
+		{
+			count += write(1, (format + i), 1);
+		}
+	}
+	va_end(list);
+
+	return (count);
+}
